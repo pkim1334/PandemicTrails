479ac40e4b72261a06d41aed667de999460d6b09
Patrick Kim <pkim1334@gmai.com>
2020-10-28 16:42:28 -0400

    healthbar and stuff
    fixed player movement and finally got healthbar to work

---
+++ PandemicTrails/Assets/player_health.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_health : MonoBehaviour
+{
+
+	public int maxHealth = 100;
+	public int currentHealth;
+
+	public health_bar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				TakeDamage(5);
+			}
+    }
+
+	void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+
+		healthBar.SetHealth(currentHealth);
+	}
+
+}
