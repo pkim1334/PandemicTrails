61d32d3a2ae0d9a2fd19c288829722100e766f0c
Rylee <rhooftllen@muhlenberg.edu>
2020-12-04 08:23:23 -0500

    Shop in progress

---
+++ PandemicTrails2/Assets/shopmenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shopmenu : MonoBehaviour
+{
+    public shop shopMenuOpen;
+
+    void start () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            shopMenuOpen.gameObject.SetActive(!shopMenuOpen.gameObject.activeSelf);
+        }
+    }
+}
